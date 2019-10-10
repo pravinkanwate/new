13ceb6a34f9a02ece1c52baeee43f95dabbc2ccf
pravin <pkanwate1234@gmail.com>
2019-10-10 19:06:59 +0530

    2nd commit

---
+++ src/main/java/com/example/prod1/Prod1Application.java
@@ -0,0 +1,13 @@
+package com.example.prod1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prod1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prod1Application.class, args);
+	}
+
+}
